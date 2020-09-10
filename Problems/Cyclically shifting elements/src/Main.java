2ff12ea94cf2838fe0c6ce398ded4bca6c0773ae
Misanty Morningstar <tugce.tatli@gmail.com>
2020-09-10 23:14:00 +0200

    regex positive lookahead side notes are added. The problem is to match at least one uppercase letter 12 characters minimum

--- Problems/Cyclically shifting elements/src/Main.java
+++ Problems/Cyclically shifting elements/src/Main.java
@@ -1,7 +1,19 @@
+import java.time.LocalDate;
+
+import java.time.LocalTime;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+
+        LocalDate date = LocalDate.ofYearDay(2018, 2);
+        date = LocalDate.of(2018, 1, 1).withDayOfMonth(1);
+        System.out.println(LocalDate.of(2018, 3, 2).minusMonths(2));
+        System.out.println(LocalTime.now().getNano());
+        System.out.println("question: "+LocalTime.MIDNIGHT);
+
+        System.out.println(LocalTime.of(0,1,1,999999));
+
         Scanner scanner = new Scanner(System.in);
         int l = scanner.nextInt();
         int cnt = 1;
